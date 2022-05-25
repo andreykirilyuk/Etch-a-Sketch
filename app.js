97d1314b238b2a180977fdb0021bdf1e55a261f8
Andrey <andre.kirilyuk@gmail.com>
2022-05-25 18:12:34 -0400

    Prompt added Add prompt button asking for size of grid.

--- app.js
+++ app.js
@@ -1,15 +1,40 @@
 const container = document.querySelector('.container');
 const grid = document.querySelector('.grid');
 let squares = document.querySelectorAll('grid .div');
+let size = 0;
+let newGrid = 256;
 
-window.onload=function() {
-    for (let i=0;i<256;i++){
-        const square = document.createElement('div');
-        grid.appendChild(square);
-        square.classList.add('square');
+// window.onload=function createGrid(newGrid) {
+//     for (let i=0;i<newGrid;i++){
+//         const square = document.createElement('div');
+//         grid.appendChild(square);
+//         square.classList.add('square');
+//     }
+// }
+
+function createGrid(newGrid) {
+        for (let i=0;i<newGrid;i++){
+            const square = document.createElement('div');
+            grid.appendChild(square);
+            square.classList.add('square');
+        }
     }
+
+function clearGrid (){
+    grid.innerHTML = ''   
 }
 
 grid.addEventListener('mouseover', function handleHover(e) {
     e.target.classList.add('hovered');}
 );
+
+const button = document.querySelector('.btn');
+button.addEventListener('click', () => {
+    clearGrid();
+    size = prompt('How many squares would you like per side? (Max: 100)');
+    newGrid = (size*size);
+    createGrid(newGrid);
+}
+);
+
+createGrid(newGrid);
